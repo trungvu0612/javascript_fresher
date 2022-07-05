68e741c91bb08582f4bf1df91fe95e9ba96e1158
Vu Thanh Trung <trung.vu-thanh@internal.banvien.com>
2022-07-05 15:35:17 +0700

    Conditionals done

---
+++ src/Conditionals/level3.ts
@@ -0,0 +1,43 @@
+// // Exercises: Level 3
+// 1. Write a program which tells the number of days in a month.
+// let enterDayMonth: string | null = prompt("Enter a month:")
+// switch (enterDayMonth) {
+//     case "January":
+//         console.log("January has 31 days.")
+//         break
+//     case "February":
+//         console.log("February has 28 days.")
+//         break
+//     case "March":
+//         console.log("March has 31 days.")
+//         break
+//     case "April":
+//         console.log("April has 30 days.")
+//         break
+//     case "May":
+//         console.log("May has 31 days.")
+//         break
+//     case "June":
+//         console.log("June has 30 days.")
+//         break
+//     case "July":
+//         console.log("July  has 31 days.")
+//         break
+//     case "August":
+//         console.log("August has 31 days.")
+//         break
+//     case "September":
+//         console.log("September has 30 days.")
+//         break
+//     case "October ":
+//         console.log("October  has 31 days.")
+//         break
+//     case "November":
+//         console.log("November has 30 days.")
+//         break
+//     case "December":
+//         console.log("December has 31 days.")
+//         break
+//     default:
+//         console.log("please enter again")
+// }
--- src/array/level2.ts
+++ src/array/level2.ts
@@ -16,10 +16,11 @@ const newString1 = newString.split(",").join("");
 console.log(newString1)
 const newString2 = newString1.split(" ")
 let i: number;
-for(i=0; i<newString2.length; i++){
-    if(newString2[i]===""){
+for (i = 0; i < newString2.length; i++) {
+    if (newString2[i] === "") {
         newString2.splice(i, 1)
-    };
+    }
+    ;
 }
 console.log(newString2, newString2.length)
 
@@ -38,22 +39,22 @@ shoppingCart.splice(3, 1, "Green tea");
 console.log(shoppingCart)
 // 4. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'.
 // If it does not exist add to the countries list.
-if(countries.includes("Ethiopia")){
+if (countries.includes("Ethiopia")) {
     let a: number;
-    for(a=0; a<countries.length;a++){
-        if(countries[i]=="Ethiopia"){
+    for (a = 0; a < countries.length; a++) {
+        if (countries[i] == "Ethiopia") {
             console.log(countries[i].toUpperCase());
         }
     }
-}else{
+} else {
     countries.push("Ethiopia");
 }
 console.log(countries);
 // 5. In the webTechs array check if Sass exists in the array and if
 // it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
-if(webTechs.includes("Sass")){
+if (webTechs.includes("Sass")) {
     console.log("Sass is a CSS preprocess");
-}else{
+} else {
     webTechs.push("Sass");
     console.log(webTechs);
 }
