50364419af0b733929b8a3472735a705a464b76f
Vu Thanh Trung <trung.vu-thanh@internal.banvien.com>
2022-07-05 10:15:02 +0700

    done array

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
-// import {countries} from "./country";
-// import {webTechs} from "./web_techs";
+import {countries} from "./country";
+import {webTechs} from "./web_techs";
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
@@ -151,19 +151,107 @@ console.log(itCompanies);
 // console.log(countries)
 // console.log(webTechs)
 
-// 2. First remove all the punctuations and change the string to array and count the number of words in the array
-let text: string = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
-console.log(text);
-const newString = text.split(".").join(" ");
-console.log(newString)
-const newString1 = newString.split(",").join("");
-console.log(newString1)
-const newString2 = newString1.split(" ")
-let i: number;
-for(i=0; i<newString2.length; i++){
-    if(newString2[i]===""){
-        newString2.splice(i, 1)
-    };
+// // 2. First remove all the punctuations and change the string to array and count the number of words in the array
+// let text: string = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+// console.log(text);
+// const newString = text.split(".").join(" ");
+// console.log(newString)
+// const newString1 = newString.split(",").join("");
+// console.log(newString1)
+// const newString2 = newString1.split(" ")
+// let i: number;
+// for(i=0; i<newString2.length; i++){
+//     if(newString2[i]===""){
+//         newString2.splice(i, 1)
+//     };
+// }
+// console.log(newString2, newString2.length)
+
+// // 3. In the following shopping cart add, remove, edit items
+// const shoppingCart: string[] = ['Milk', 'Coffee', 'Tea', 'Honey'];
+// // add
+// shoppingCart.unshift("Meat")
+// console.log(shoppingCart);
+// shoppingCart.push("Sugar");
+// console.log(shoppingCart)
+// // remove
+// shoppingCart.splice(4, 1)
+// console.log(shoppingCart)
+// // edit
+// shoppingCart.splice(3, 1, "Green tea");
+// console.log(shoppingCart)
+// // 4. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'.
+// If it does not exist add to the countries list.
+// if(countries.includes("Ethiopia")){
+//     let a: number;
+//     for(a=0; a<countries.length;a++){
+//         if(countries[i]=="Ethiopia"){
+//             console.log(countries[i].toUpperCase());
+//         }
+//     }
+// }else{
+//     countries.push("Ethiopia");
+// }
+// console.log(countries);
+// // 5. In the webTechs array check if Sass exists in the array and if
+// // it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+// if(webTechs.includes("Sass")){
+//     console.log("Sass is a CSS preprocess");
+// }else{
+//     webTechs.push("Sass");
+//     console.log(webTechs);
+// }
+// // 6. Concatenate the following two variables and store it in a fullStack variable.
+// const frontEnd: string[] = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+// const backEnd: string[] = ['Node', 'Express', 'MongoDB'];
+//
+// const fullStack: string[] = frontEnd.concat(backEnd)
+//
+// console.log(fullStack)
+
+// // Exercise: Level 3
+// // 1. The following is an array of 10 students ages: js const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+// const ages: number[] = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+// // - Sort the array and find the min and max age
+// ages.sort();
+// console.log("Min age is " + ages[0] + ",Max age is " + ages[ages.length-1]);
+// // - Find the median age(one middle item or two middle items divided by two)
+// const middleAges = Math.floor((ages.length/2))
+// console.log((ages[middleAges] + ages[middleAges+1])/2);
+// // - Find the average age(all items divided by number of items)
+// const newArr: number[] = [];
+// let b: number;
+// for(b=0;b<ages.length;b++){
+//     if(ages[b]!==ages[b+1]){
+//         newArr.push(ages[b]);
+//
+//     }
+// }
+// console.log(newArr);
+// let c: number;
+// let sum: number = 0;
+// for(c=0; c< newArr.length; c++){
+//     sum += newArr[c];
+// }
+// console.log(sum / ages.length);
+// // - Find the range of the ages(max minus min)
+// console.log(ages[ages.length-1] - ages[0]);
+// - Compare the value of (min - average) and (max - average), use abs() method
+// 1.1.Slice the first ten countries from the countries array
+console.log(countries.slice(0,10));
+// 2. Find the middle country(ies) in the countries array
+const middleCountries = Math.floor(countries.length / 2);
+console.log(countries[middleCountries]);
+// 3. Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
+if(countries.length%2==0){
+   const firstArr = countries.splice(0, countries.length/2);
+   console.log(firstArr, countries);
+}else{
+   countries.unshift("Vietnam");
+   console.log(countries)
 }
-console.log(newString2, newString2.length)
+
+
+
+
 
